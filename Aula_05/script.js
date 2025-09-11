78ae926be7f3415a3295a88a7f43c1763b901d3a
lauanda1327 <lauanda.nobre@gmail.com>
2025-09-11 17:12:13 -0300

    Adicionando aula 5

---
+++ Aula_05/script.js
@@ -0,0 +1,29 @@
+console.log('Hello World!') // console.log == print
+nome = prompt('Qual o seu nome') // prompt == input, no python
+console.log('olá' + nome)
+
+a = 1
+b = 2
+
+// estrutura condicional
+if (a < b) {
+    //
+}
+else if(a = b){
+    //
+}
+else{
+    //
+}
+
+// estrutura de repetição enquanto
+i = 0
+while (i < 10) {
+    console.log(i)
+    i += 1 // i = i + 1
+}
+
+// estrutura de repetição for
+for (let i = 0; i < array.length; i++) {
+    console.log(i)
+}
