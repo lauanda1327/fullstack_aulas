73ddff27a602417339292b2bd9ea10fb6ddb86ab
lauanda1327 <lauanda.nobre@gmail.com>
2025-09-18 16:59:40 -0300

    Atividade - Adivinhação

---
+++ atividades/advinhacao/script.js
@@ -0,0 +1,28 @@
+
+let numero = Math.random()
+numero = numero * 100
+numero= Math.floor(numero) 
+console.log(numero)
+numeros_pequenos = ''
+numeros_grandes = ''
+
+function validarNumero(){
+    let numero_usuario = parseInt(document.getElementById('numero').value)
+    console.log(numero_usuario)
+    if(numero_usuario == numero){
+        document.getElementById("mensagem").innerHTML = "Número Igual"
+        document.getElementById("mensagem").style.setProperty("background-color", "#00eb00")
+    }
+    if(numero_usuario < numero){
+        numeros_pequenos = numeros_pequenos + ' - ' + numero_usuario
+        document.getElementById('resp_tentativas_pequenas').innerHTML = numeros_pequenos + ' - '
+        document.getElementById("mensagem").innerHTML = "Número Muito Pequeno"
+        document.getElementById("mensagem").style.setProperty("background-color", "#ff2424")
+    }
+    if(numero_usuario > numero){
+        numeros_grandes = numeros_grandes + ' - ' + numero_usuario
+        document.getElementById('resp_tentativas_grandes').innerHTML = numeros_grandes + ' - '
+        document.getElementById("mensagem").innerHTML = "Número Muito Grande"
+        document.getElementById("mensagem").style.setProperty("background-color", "#ff2424")
+    }
+}
